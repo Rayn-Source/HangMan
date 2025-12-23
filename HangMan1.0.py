1dca0d463f8725ee2b4ba3d96283cde99a26954f
Ryan Neo <rxyz1234@gmail.com>
2025-12-23 10:44:02 +0800

    Mock Up Draft 1.0

--- HangMan1.0.py
+++ HangMan1.0.py
@@ -1,49 +1,35 @@
-#The code starts by importing the random module.
-import random
-from collections import counter
+#1. Welcome to HangMan! And initiliazation 
+print("Welcome to HangMan")
+print_array = []
+blank = ""
+count = 5
 
-someWords = '''apple banana mango strawberry 
-orange grape pineapple apricot lemon coconut watermelon 
-cherry papaya berry peach lychee muskmelon'''
+#2. Choose a number between 1-17 to choose a word from array
+someWords = ["apple, banana, mango, strawberry,orange,grape, pineapple, apricot, lemon, coconut, watermelon,cherry, papaya, berry, peach, lychee, muskmelon"]
 
-#This module provides a way to generate random numbers.
+try:
+    value = int(input("Enter a number between 1-17 to choose a word from array"))
+except:
+    print("Please enter a number and not string")
 
-#Next, the code creates someWords, which is a list of fruit names.
+chosenWord = someWords[value -1]
+#3. Choose a letter 
+chosenWord_array = list(chosenWord)
+for letters in chosenWord_array:
+    if(input("Please enter a letter/character") == letters):
+        blank += "_"
+        print_array = list(blank)       
 
-#The list is split into spaces using the string ' ', and then each space is replaced with a letter.
+#4.If a letter is choosen, if it is not right it will display hangman
+    else:
+        print(count - 1)
 
-#Next, the code randomly selects a secret word from our someWords list.
+#5.If the letter of the word in the array is correct it will display on the terminal
+print(' '.join(print_array))
 
-#This word will be used as the input for the game later on.
 
-#The next part of the code checks to see if the user has entered an alpha character (a letter that appears in front of other letters).
-
-#If not, then they are asked to enter only a letter.
-
-#If they enter an alpha character, then it's assumed that they want to guess at another letter in word .
-
-#So, this part of the code checks to see if guess matches one of the letters in word .
-
-#If it does, then chances is updated and flag is set to 1 .
-
-#Otherwise, chances is decreased by 1 and flag remains at 0 .
-
-#The next part of the code tries to guess at another letter in word .
-
-#If guess isn't valid (i.e., it doesn't match any of the letters in word ), then print() prints out all empty spaces for letters in word , and
-
-#The code starts by importing the random module.
-
-#This module provides us with a number of useful functions, one of which is the choice function.
-
-#This function allows us to randomly choose a secret word from our list of words.
-
-#Next, we create some variables which will be used throughout the program.
-
-#These include someWords , word and letterGuessed .
-
-#letterGuessed will store the letter guessed by the player, while chances will store the number of times that the player has correctly guessed the word so far.
-
-#correct will keep track of how many letters have been guessed so far and flag will indicate whether or not the player has guessed the word correctly.
-
-#We then start looping through our list of words and randomly choosing a secret word from it.
+if(count <=0):
+    print("Game Over")
+else:
+    print("Congrats you guessed the word")
+#6.Congrats!
