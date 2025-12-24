62db3093408e3ba4de758a483c237a91fa5bd448
Ryan Neo <rxyz1234@gmail.com>
2025-12-24 13:48:16 +0800

    Updated from YouTube

--- HangMan2.0.py
+++ HangMan2.0.py
@@ -1,59 +1,34 @@
-#The code starts by importing the random module.
-import random
-from collections import counter
+import pygame,random, sys
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+font = pygame.font.Font(None, 40)
+word = random.choice(['python', 'pygame', 'hangman'])
+guessed, wrong = ['_'] * len(word), []
+attempts = 6
 
-someWords = ["apple, banana, mango, strawberry,orange,grape, pineapple, apricot, lemon, coconut, watermelon,cherry, papaya, berry, peach, lychee, muskmelon"]
+def draw_hangman(strikes):
+    parts = [(150, 400, 150, 150), (100, 400, 200, 400), (150, 150, 250, 150), (250, 150, 250, 200),
+             (250, 225, 25), (250, 250, 250, 320), (250, 270, 220, 320), (250, 270, 280, 320),
+             (250, 320, 220, 380), (250, 320, 280, 380)]
+    for i in range(strikes): pygame.draw.line(screen, (0, 0, 0), parts[i][:2], parts[i][2:], 5) if i != 8 else pygame.draw.circle(screen, (0, 0, 0), parts[i][:2], parts[i][2], 5)
 
-#This module provides a way to generate random numbers.
+while attempts > 0 and '_' in guessed:
+    screen.fill((255, 255, 255))
+    screen.blit(font.render(' '.join(guessed), True, (0, 0, 0)), (100, 100))
+    screen.blit(font.render('Wrong: ' + ' '.join(wrong), True, (255, 0, 0)), (100, 400))
+    draw_hangman(6 - attempts)
+    pygame.display.flip()
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            guess = chr(event.key)
+            if guess in word and guess not in guessed:
+                for i, letter in enumerate(word):
+                    if letter == guess: guessed[i] = guess
+            elif guess not in wrong: wrong.append(guess); attempts -= 1
 
-# randomly choose a secret word from our "someWords" LIST.
-word = random.choice(someWords)
-
-#Next, the code creates someWords, which is a list of fruit names.
-
-if __name__ == '__main__':
-    print('Guess the word! HINT: word is a name of a fruit')
-
-#The list is split into spaces using the string ' ', and then each space is replaced with a letter.
-    for i in word:
-        # For printing the empty spaces for letters of the word
-        print('_', end=' ')
-    print()
-#Next, the code randomly selects a secret word from our someWords list.
-
-
-
-#This word will be used as the input for the game later on.
-
-#The next part of the code checks to see if the user has entered an alpha character (a letter that appears in front of other letters).
-
-#If not, then they are asked to enter only a letter.
-
-#If they enter an alpha character, then it's assumed that they want to guess at another letter in word .
-
-#So, this part of the code checks to see if guess matches one of the letters in word .
-
-#If it does, then chances is updated and flag is set to 1 .
-
-#Otherwise, chances is decreased by 1 and flag remains at 0 .
-
-#The next part of the code tries to guess at another letter in word .
-
-#If guess isn't valid (i.e., it doesn't match any of the letters in word ), then print() prints out all empty spaces for letters in word , and
-
-#The code starts by importing the random module.
-
-#This module provides us with a number of useful functions, one of which is the choice function.
-
-#This function allows us to randomly choose a secret word from our list of words.
-
-#Next, we create some variables which will be used throughout the program.
-
-#These include someWords , word and letterGuessed .
-
-#letterGuessed will store the letter guessed by the player, while chances will store the number of times that the player has correctly guessed the word so far.
-
-#correct will keep track of how many letters have been guessed so far and flag will indicate whether or not the player has guessed the word correctly.
-
-#We then start looping through our list of words and randomly choosing a secret word from it.
+screen.fill((255, 255, 255))
+screen.blit(font.render('You Win!' if '_' not in guessed else 'You Lose!', True, (255, 0, 0)), (150, 200))
+pygame.display.flip()
+pygame.time.wait(2000)
